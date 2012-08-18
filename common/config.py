66227f6a8810ce3c9209a0f1521551ccd7d41772
Weilin <mzweilin@gmail.com>
2012-08-18 17:15:32 +0800

    Added support to load the globalpot configurations.

--- common/config.py
+++ common/config.py
@@ -9,22 +9,30 @@ config = {
 
 def parse_config(cfg):
     values = {}
-    try:
-        # essential config items
+    
+    if cfg.has_option('main', 'name'):
         values['name'] = cfg.get('main', 'name')
-        values['iface'] = cfg.get('main', 'interface')
-        values['mac'] = cfg.get('main', 'mac')
-        values['ndp'] = cfg.getint('IPv6', 'ndp')
-        values['uecho'] = cfg.getint('IPv6', 'icmpv6_echo_unicast')
-        values['mecho'] = cfg.getint('IPv6', 'icmpv6_echo_multicast')
-        values['iv_ext_hdr'] = cfg.getint('IPv6', 'icmpv6_invalid_exheader')
-        values['slaac'] = cfg.getint('IPv6', 'slaac')
-        values['dhcpv6'] = cfg.getint('IPv6', 'dhcpv6')
-    except (NoSectionError, NoOptionError, ValueError), err:
-        raise ParsingError(str(err))
-
-    if cfg.has_option('IPv6', 'tcp_ports'):
-        values['tcp_ports'] = map(int, cfg.get('IPv6', 'tcp_ports').split(','))
-    if cfg.has_option('IPv6', 'udp_ports'):
-        values['udp_ports'] = map(int, cfg.get('IPv6', 'udp_ports').split(','))
+    if values.has_key('name') and values['name'] == "Globalpot":
+        try:
+            # essential config items
+            values['iface'] = cfg.get('main', 'interface')
+        except (NoSectionError, NoOptionError, ValueError), err:
+            raise ParsingError(str(err))
+    else: # Honeypot
+        try:
+            # essential config items
+            values['iface'] = cfg.get('main', 'interface')
+            values['mac'] = cfg.get('main', 'mac')
+            values['ndp'] = cfg.getint('IPv6', 'ndp')
+            values['uecho'] = cfg.getint('IPv6', 'icmpv6_echo_unicast')
+            values['mecho'] = cfg.getint('IPv6', 'icmpv6_echo_multicast')
+            values['iv_ext_hdr'] = cfg.getint('IPv6', 'icmpv6_invalid_exheader')
+            values['slaac'] = cfg.getint('IPv6', 'slaac')
+            values['dhcpv6'] = cfg.getint('IPv6', 'dhcpv6')
+        except (NoSectionError, NoOptionError, ValueError), err:
+            raise ParsingError(str(err))
+        if cfg.has_option('IPv6', 'tcp_ports'):
+            values['tcp_ports'] = map(int, cfg.get('IPv6', 'tcp_ports').split(','))
+        if cfg.has_option('IPv6', 'udp_ports'):
+            values['udp_ports'] = map(int, cfg.get('IPv6', 'udp_ports').split(','))
     config.update(values)
